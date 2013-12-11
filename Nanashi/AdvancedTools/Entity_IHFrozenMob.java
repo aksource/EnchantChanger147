2d9d5a5ac2afff1298cb9db78e6b0adcac62e360
aksource <akira.mew@gmail.com>
2013-12-11 21:50:05 +0900

    AT:Fix01

--- Nanashi/AdvancedTools/Entity_IHFrozenMob.java
+++ Nanashi/AdvancedTools/Entity_IHFrozenMob.java
@@ -1,21 +1,24 @@
 package Nanashi.AdvancedTools;
 
+import java.util.List;
+
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.ai.EntityAITasks;
 import net.minecraft.entity.monster.EntityEnderman;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
+import cpw.mods.fml.common.ObfuscationReflectionHelper;
 
 public class Entity_IHFrozenMob extends Entity
 {
 	EntityLiving frozen;
 	EntityPlayer player;
+	EntityAITasks entityTasks;
 	int FrozenRest;
-//	int maxHealth;
 
 	public Entity_IHFrozenMob(World var1)
 	{
@@ -25,14 +28,10 @@ public class Entity_IHFrozenMob extends Entity
 	public Entity_IHFrozenMob(World var1, EntityLiving var2, EntityPlayer var3)
 	{
 		super(var1);
-
-		if (var2 == null)
-		{
+		if (var2 == null){
 			this.setDead();
 			this.frozen = null;
-		}
-		else
-		{
+		}else{
 			this.frozen = var2;
 			this.player = var3;
 			this.setSize(this.frozen.width, this.frozen.height);
@@ -40,71 +39,66 @@ public class Entity_IHFrozenMob extends Entity
 			this.setPosition(this.frozen.posX, this.frozen.posY, this.frozen.posZ);
 			this.rotationYaw = this.frozen.rotationYaw;
 
-			if (this.frozen.isBurning())
-			{
+			if (this.frozen.isBurning()){
 				this.setDead();
 				this.frozen.setFire(0);
 				this.frozen = null;
-			}
-			else
-			{
+			}else{
 				this.frozen.setFire(0);
-//				List var3 = var1.getEntitiesWithinAABB(Entity_IHFrozenMob.class, this.boundingBox.expand(1.0D, 1.0D, 1.0D));
-//
-//				for (int var4 = 0; var4 < var3.size(); ++var4)
-//				{
-//					if (var3.get(var4) instanceof Entity_IHFrozenMob)
-//					{
-//						this.setDead();
-//						this.frozen = null;
-//						return;
-//					}
-//				}
-
-				if (this.frozen instanceof EntityMob)
-				{
-					this.frozen.attackTime = 200;
-					this.frozen.addPotionEffect(new PotionEffect(15,200));
-					((EntityMob)this.frozen).setAttackTarget(this.frozen);
-					((EntityMob)this.frozen).setLastAttackingEntity(this.frozen);
+//				this.originalMethodDoubleEntity(var1);
+				if (this.frozen instanceof EntityMob){
+//					this.altenativeMethodForAI();
+					this.originalMethodForAI();
 				}
-
 				this.FrozenRest = 200;
 			}
 		}
 	}
+	private void originalMethodDoubleEntity(World var1)
+	{
+		List var3 = var1.getEntitiesWithinAABB(Entity_IHFrozenMob.class, this.boundingBox.expand(1.0D, 1.0D, 1.0D));
 
-	/**
-	 * Called to update the entity's position/logic.
-	 */
+		for (int var4 = 0; var4 < var3.size(); ++var4)
+		{
+			if (var3.get(var4) instanceof Entity_IHFrozenMob)
+			{
+				this.setDead();
+				this.frozen = null;
+				return;
+			}
+		}
+	}
+	private void originalMethodForAI()
+	{
+		this.entityTasks = this.frozen.tasks;
+		ObfuscationReflectionHelper.setPrivateValue(EntityLiving.class, this.frozen, new EntityAITasks(this.worldObj.theProfiler), 59);
+	}
+	private void altenativeMethodForAI()
+	{
+		this.frozen.attackTime = 200;
+		this.frozen.addPotionEffect(new PotionEffect(15,200));
+		((EntityMob)this.frozen).setAttackTarget(this.frozen);
+		((EntityMob)this.frozen).setLastAttackingEntity(this.frozen);
+	}
+
 	public void onUpdate()
 	{
 		--this.FrozenRest;
 
-		if (this.frozen == null)
-		{
+		if (this.frozen == null){
 			this.setDead();
-		}
-		else if (!this.frozen.isBurning() && this.FrozenRest >= 0/* && (this.frozen.hurtTime == 0 || this.FrozenRest >= 190)*/)
-		{
-			if (this.frozen.getHealth() > 0 && !this.frozen.isDead && !(this.frozen instanceof EntityEnderman))
-			{
+		}else if (!this.frozen.isBurning() && (this.frozen.hurtTime == 0 || this.FrozenRest >= 190)){
+			if (this.frozen.getHealth() > 0 && !(this.frozen instanceof EntityEnderman)){
 				this.frozen.setPosition(this.posX, this.frozen.posY, this.posZ);
-				this.setPosition(this.frozen.posX, this.frozen.posY, this.frozen.posZ);
 				this.rotationYaw = this.frozen.rotationYaw;
 				this.frozen.onGround = false;
-				if(this.worldObj.isRemote)
-					this.frozen.setVelocity(0.0D, this.frozen.motionY, 0.0D);
-
-				if (this.frozen instanceof EntityMob)
-				{
+				if (this.frozen instanceof EntityMob){
 					((EntityMob)this.frozen).attackTime = 20;
 					((EntityMob)this.frozen).setAttackTarget(this.frozen);
 					((EntityMob)this.frozen).setLastAttackingEntity(this.frozen);
 				}
 
-				for (int var1 = 0; var1 < 2; ++var1)
-				{
+				for (int var1 = 0; var1 < 2; ++var1){
 					double var2 = this.rand.nextDouble() * (double)this.width;
 					double var4 = this.rand.nextDouble() * Math.PI * 1.0D;
 					double var6 = this.posX + var2 * Math.sin(var4);
@@ -112,55 +106,39 @@ public class Entity_IHFrozenMob extends Entity
 					double var10 = this.posZ + var2 * Math.cos(var4);
 					this.worldObj.spawnParticle("explode", var6, var8, var10, 0.0D, 0.0D, 0.0D);
 				}
-			}
-			else
-			{
+			}else{
 				this.setDead();
 			}
-		}
-		else
-		{
+		}else{
 			this.setDead();
 			this.worldObj.playSoundEffect(this.posX, this.posY, this.posZ, "random.glass", 1.0F, this.worldObj.rand.nextFloat() * 0.1F + 0.9F);
 		}
 	}
 
-	 /**
-	  * Will get destroyed next tick.
-	  */
 	 public void setDead()
 	 {
-		 if(!this.worldObj.isRemote && this.frozen instanceof EntityMob)
-		 {
+		 if(this.entityTasks != null){
+			 ObfuscationReflectionHelper.setPrivateValue(EntityLiving.class, this.frozen, this.entityTasks, 59);
 			 ((EntityMob)this.frozen).setAttackTarget(this.player);
 			 ((EntityMob)this.frozen).setLastAttackingEntity(this.player);
 		 }
 		 super.setDead();
 	 }
 
-	 /**
-	  * Gets how bright this entity is.
-	  */
 	 public float getBrightness(float var1)
 	 {
-//		 return this.frozen == null ? 0.0F : (float)this.frozen.getBrightnessForRender(var1);
 		 return this.frozen == null ? 0.0F : 1.0f;
 	 }
 
-//	 public int getMaxHealth()
-//	 {
-//		 return 2;
-//	 }
-
+	 public boolean canBeCollidedWith()
+	 {
+		 return false;
+	 }
 	 protected void entityInit() {}
-
-	 /**
-	  * (abstract) Protected helper method to read subclass entity data from NBT.
-	  */
-	 protected void readEntityFromNBT(NBTTagCompound var1) {}
-
-	 /**
-	  * (abstract) Protected helper method to write subclass entity data to NBT.
-	  */
-	 protected void writeEntityToNBT(NBTTagCompound var1) {}
+	 protected void readEntityFromNBT(NBTTagCompound var1) {
+		 this.FrozenRest = var1.getInteger("frozenRest");
+	 }
+	 protected void writeEntityToNBT(NBTTagCompound var1) {
+		 var1.setInteger("frozenRest", FrozenRest);
+	 }
 }
--- Nanashi/AdvancedTools/ItemUQIceHold.java
+++ Nanashi/AdvancedTools/ItemUQIceHold.java
@@ -42,8 +42,7 @@ public class ItemUQIceHold extends ItemUniqueArms
 	{
 		super.onUpdate(var1, var2, var3, var4, var5);
 
-		if (this.coolTime > 0)
-		{
+		if (this.coolTime > 0){
 			--this.coolTime;
 		}
 	}
@@ -52,8 +51,7 @@ public class ItemUQIceHold extends ItemUniqueArms
 	{
 		int var5 = var3.getFoodStats().getFoodLevel();
 
-		if (var5 > 6)
-		{
+		if (var5 > 6){
 			int var6 = this.getMaxItemUseDuration(var1) - var4;
 			float var7 = (float)var6 / 20.0F;
 			var7 = (var7 * var7 + var7 * 2.0F) / 3.0F;
@@ -62,34 +60,25 @@ public class ItemUQIceHold extends ItemUniqueArms
 			int var12;
 			int var13;
 
-			if (var7 >= 1.0F)
-			{
+			if (var7 >= 1.0F){
 				int var8 = (int)var3.posX;
 				int var9 = (int)var3.posY - 2;
 				var10 = (int)var3.posZ;
 
-				if (!var2.canMineBlock(var3, var8, var9, var10))
-				{
+				if (!var2.canMineBlock(var3, var8, var9, var10)){
 					return;
 				}
 
-				for (var11 = -1; var11 <= 1; ++var11)
-				{
-					for (var12 = -6; var12 <= 6; ++var12)
-					{
-						for (var13 = -6; var13 <= 6; ++var13)
-						{
-							if (Math.sqrt((double)(var12 * var12 + var13 * var13)) <= 5.8D && var2.isAirBlock(var8 + var12, var9 + 1 + var11, var10 + var13))
-							{
+				for (var11 = -1; var11 <= 1; ++var11){
+					for (var12 = -6; var12 <= 6; ++var12){
+						for (var13 = -6; var13 <= 6; ++var13){
+							if (Math.sqrt((double)(var12 * var12 + var13 * var13)) <= 5.8D && var2.isAirBlock(var8 + var12, var9 + 1 + var11, var10 + var13)){
 								var2.spawnParticle("explode", (double)(var8 + var12), (double)(var9 + 1 + var11), (double)(var10 + var13), 0.0D, 0.0D, 0.0D);
 								Material var14 = var2.getBlockMaterial(var8 + var12, var9 + var11, var10 + var13);
 
-								if (var14 == Material.water && var2.getBlockMetadata(var8, var9 + var11, var10) == 0)
-								{
+								if (var14 == Material.water && var2.getBlockMetadata(var8, var9 + var11, var10) == 0){
 									var2.setBlockWithNotify(var8 + var12, var9 + var11, var10 + var13, Block.ice.blockID);
-								}
-								else if (var2.getBlockId(var8 + var12, var9 + 1 + var11, var10 + var13) == 0 && Block.snow.canPlaceBlockAt(var2, var8 + var12, var9 + 1 + var11, var10 + var13))
-								{
+								}else if (var2.getBlockId(var8 + var12, var9 + 1 + var11, var10 + var13) == 0 && Block.snow.canPlaceBlockAt(var2, var8 + var12, var9 + 1 + var11, var10 + var13)){
 									var2.setBlockWithNotify(var8 + var12, var9 + 1 + var11, var10 + var13, Block.snow.blockID);
 								}
 							}
@@ -99,89 +88,66 @@ public class ItemUQIceHold extends ItemUniqueArms
 
 				List var19 = var2.getEntitiesWithinAABB(EntityLiving.class, var3.boundingBox.expand(5.0D, 1.0D, 5.0D));
 
-				for (var12 = 0; var12 < var19.size(); ++var12)
-				{
+				for (var12 = 0; var12 < var19.size(); ++var12){
 					EntityLiving var21 = (EntityLiving)var19.get(var12);
 					DamageSource var24 = DamageSource.causePlayerDamage(var3);
 
-					if (var21 instanceof EntityEnderman)
-					{
+					if (var21 instanceof EntityEnderman){
 						var21.attackEntityFrom(var24, this.weaponStrength * 3);
-					}
-					else if (var21 != var3)
-					{
+					}else if (var21 != var3){
 						var21.attackEntityFrom(var24, 0);
 						Entity_IHFrozenMob var15 = new Entity_IHFrozenMob(var2, var21, var3);
 
-						if (!var2.isRemote)
-						{
+						if (!var2.isRemote){
 							var2.spawnEntityInWorld(var15);
 						}
 					}
 				}
-			}
-			else
-			{
+			}else{
 				MovingObjectPosition var16 = AdvancedTools.setMousePoint(var2, var3);
 				boolean var17 = false;
 
-				if (var16 != null && var16.typeOfHit == EnumMovingObjectType.ENTITY)
-				{
+				if (var16 != null && var16.typeOfHit == EnumMovingObjectType.ENTITY){
 					Entity var18 = var16.entityHit;
 
-					if (var18 instanceof EntityLiving)
-					{
+					if (var18 instanceof EntityLiving){
 						DamageSource var20 = DamageSource.causePlayerDamage(var3);
 
-						if (var18 instanceof EntityEnderman)
-						{
+						if (var18 instanceof EntityEnderman){
 							var18.attackEntityFrom(var20, this.weaponStrength * 3);
-						}
-						else
-						{
+						}else{
 							var18.attackEntityFrom(var20, 0);
 							Entity_IHFrozenMob var22 = new Entity_IHFrozenMob(var2, (EntityLiving)var18, var3);
 
-							if (!var2.isRemote)
-							{
+							if (!var2.isRemote){
 								var2.spawnEntityInWorld(var22);
 							}
 						}
-
 						var17 = true;
 					}
 				}
 
-				if (!var17)
-				{
+				if (!var17){
 					var16 = this.getMovingObjectPositionFromPlayer(var2, var3, true);
 
-					if (var16 == null)
-					{
+					if (var16 == null){
 						return;
 					}
 
-					if (var16.typeOfHit == EnumMovingObjectType.TILE)
-					{
+					if (var16.typeOfHit == EnumMovingObjectType.TILE){
 						var10 = var16.blockX;
 						var11 = var16.blockY;
 						var12 = var16.blockZ;
 
-						for (var13 = -3; var13 <= 3; ++var13)
-						{
-							for (int var25 = -3; var25 <= 3; ++var25)
-							{
-								if (Math.sqrt((double)(var13 * var13 + var25 * var25)) <= 2.8D && var2.isAirBlock(var10 + var13, var11 + 1, var12 + var25))
-								{
+						for (var13 = -3; var13 <= 3; ++var13){
+							for (int var25 = -3; var25 <= 3; ++var25){
+								if (Math.sqrt((double)(var13 * var13 + var25 * var25)) <= 2.8D && var2.isAirBlock(var10 + var13, var11 + 1, var12 + var25)){
 									var2.spawnParticle("explode", (double)(var10 + var13), (double)(var11 + 1), (double)(var12 + var25), 0.0D, 0.0D, 0.0D);
 									Material var23 = var2.getBlockMaterial(var10 + var13, var11, var12 + var25);
 
-									if (var23 == Material.water && var2.getBlockMetadata(var10, var11, var12) == 0)
-									{
+									if (var23 == Material.water && var2.getBlockMetadata(var10, var11, var12) == 0){
 										var2.setBlockWithNotify(var10 + var13, var11, var12 + var25, Block.ice.blockID);
-									}
-									else if (var2.getBlockId(var10 + var13, var11 + 1, var12 + var25) == 0 && Block.snow.canPlaceBlockAt(var2, var10 + var13, var11 + 1, var12 + var25))
-									{
+									}else if (var2.getBlockId(var10 + var13, var11 + 1, var12 + var25) == 0 && Block.snow.canPlaceBlockAt(var2, var10 + var13, var11 + 1, var12 + var25)){
 										var2.setBlockWithNotify(var10 + var13, var11 + 1, var12 + var25, Block.snow.blockID);
 									}
 								}
@@ -193,8 +159,7 @@ public class ItemUQIceHold extends ItemUniqueArms
 
 			var1.damageItem(1, var3);
 
-			if (!var3.capabilities.isCreativeMode)
-			{
+			if (!var3.capabilities.isCreativeMode){
 				var3.getFoodStats().addStats(-1, 1.0f);
 			}
 
@@ -218,8 +183,7 @@ public class ItemUQIceHold extends ItemUniqueArms
 	{
 		int var4 = var3.getFoodStats().getFoodLevel();
 
-		if (var4 > 6)
-		{
+		if (var4 > 6){
 			var3.setItemInUse(var1, this.getMaxItemUseDuration(var1));
 		}
 
